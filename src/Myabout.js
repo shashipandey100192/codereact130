7a72ec5af41c795f84391cb1bcbf16a11e8ba464
shashiweb <shashiweb64@gmail.com>
2024-06-13 14:19:08 +0530

    create new project

---
+++ src/Myabout.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+function About()
+{
+    return <h1>welcome to react.js</h1>
+}
+
+export default About
+
+export function About1()
+{
+    return(
+        <Fragment>
+        <h2>this is heading two</h2>
+        <h2>sdgfhdsgfhsdgfsdf</h2>
+        </Fragment>
+    )
+ 
+}
+
+
